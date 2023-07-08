57fbc2cb6e8e042e1381075f4d223fb64c497a2a
jaya krishna <jayakrishnaswamy.g@gmail.com>
2023-07-08 15:41:05 +0530

    add style fixed

--- src/Components/CreateOrModifyNoteComp/AddCard.js
+++ src/Components/CreateOrModifyNoteComp/AddCard.js
@@ -1,5 +1,5 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Button, Input , Form, message, Badge, Alert} from 'antd';
+import { Button, Input , Form, message, Badge, Alert, Col, Row} from 'antd';
 import { useEffect, useState } from 'react';
 import FireCardForNotification from '../FireCardComp/FireCardForNotification';
 import axios from 'axios';
@@ -102,6 +102,7 @@ const  AddCard = (props) =>{
 
     }
 
+    
 
 
         return(
@@ -112,213 +113,221 @@ const  AddCard = (props) =>{
                 <div>
                 <h1> Add new fire card </h1>
 
-                <div style={{display : 'grid', gridTemplateColumns : "60vw 40vw"}}>
-                   
+                <Row gutter={[8, 8]}>
+                <Col xs={{ span: 24  }} lg={{ span: 18  }}>
+
+
                 <div>
 
-                        <Form name="basic" labelCol={{  span: 8, }}
-                        wrapperCol={{ span: 16, }}
-                        style={{  maxWidth: "70vw", margin: "30px" }}
-                        initialValues={{ remember: true,  }}
-                        onFinish={addNote}
-                    
-                        autoComplete="off" >
-                        
-                        
-                        <Form.Item
-                        label="Title"
-                        name="title"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input note title',
-                            },
-                        ]} >
-                        
-                        <Input allowClear  placeholder="Note title" value={title} 
-                            onChange={(event)=>{ inputChanged(event, "title") } }/>
+                    <Form name="basic" labelCol={{  span: 8, }}
+                    wrapperCol={{ span: 16, }}
+                    style={{  maxWidth: "70vw", margin: "30px" }}
+                    initialValues={{ remember: true,  }}
+                    onFinish={addNote}
 
-                        </Form.Item>
+                    autoComplete="off" >
 
 
+                    <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input note title',
+                        },
+                    ]} >
 
-                        <Form.Item
-                        label="Owner of note"
-                        name="owner"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input owner name',
-                            },
-                        ]} >
-                        
-                        <Input allowClear  size="large"  placeholder="Owner name" value={owner} 
-                        prefix={<UserOutlined />} onChange={(event)=>{ inputChanged(event, "owner") } } />
+                    <Input allowClear  placeholder="Note title" value={title} 
+                        onChange={(event)=>{ inputChanged(event, "title") } }/>
 
-                        </Form.Item>
-
-
-                        <Form.Item
-                        label="Primary image url of note"
-                        name="Primary image url of note"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input primary image url',
-                            },
-                        ]} >
-                        
-                        <Search  placeholder="image url" allowClear  enterButton="Search image"  size="large" value={imageRef} 
-                        onChange={(event)=>{ inputChanged(event, "imageRef") } }/>
-
-                        </Form.Item>
-
-                        <Form.Item
-                        label="Additional image url of note "
-                        name="Additional image url of note"
-                        rules={[
-                            {
-                            required: false,
-                            message: 'Please input your additional image url !',
-                            },
-                        ]} >
-
-                        <TextArea showCount maxLength={1000} style={{ height: 120, marginBottom: 24 }} 
-                            placeholder="semicolon ';' seperated" value={addtionalImgs}onChange={(event)=>{ inputChanged(event, "addtionalImgs") } }/>
-                        
-
-                        </Form.Item>
-
-
-                        <Form.Item
-                        label="Reference URL"
-                        name="Reference URL"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input reference URL',
-                            },
-                        ]} >
-                        
-                        <Search  placeholder="reference url" allowClear  enterButton="Search image"  size="large" value={url} 
-                        onChange={(event)=>{ inputChanged(event, "url") } }/>
-
-                        </Form.Item>
-
-                        <Form.Item
-                        label="Short desc"
-                        name="Short desc"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input short desc',
-                            },
-                        ]} >
-                        
-                        <TextArea showCount maxLength={100} style={{ height: 120, marginBottom: 24 }} 
-                            placeholder="Short desc" value={shortDesc}onChange={(event)=>{ inputChanged(event, "shortDesc") } }/>
-
-                        </Form.Item>
-
-
-                        <Form.Item
-                        label="Full desc"
-                        name="Full desc"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input full desc',
-                            },
-                        ]} >
-                        
-                        <TextArea showCount maxLength={10000} style={{ height: 120, marginBottom: 24 }} 
-                            placeholder="Short desc" value={fullDesc}onChange={(event)=>{ inputChanged(event, "fullDesc") } }/>
-
-                        </Form.Item>
-
-
-                        <Form.Item
-                        label="Category"
-                        name="Category"
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Please input Category',
-                            },
-                        ]} >
-                        
-                        <TextArea showCount maxLength={100} style={{ height: 120, marginBottom: 24 }} 
-                            placeholder="Short desc" value={category}onChange={(event)=>{ inputChanged(event, "category") } }/>
-
-                        </Form.Item>
-
-                        {/* secureInfo */}
-
-                        <Form.Item
-                        label="Secure info"
-                        name="Secure info"
-                        rules={[
-                            {
-                            required: false,
-                            message: 'Please input Secure info',
-                            },
-                        ]} >
-                        
-                        <TextArea showCount maxLength={10000} style={{ height: 120, marginBottom: 24 }} 
-                            placeholder="Short desc" value={secureInfo}onChange={(event)=>{ inputChanged(event, "secureInfo") } }/>
-
-                        </Form.Item>
-                        
-                        <Form.Item
-                        label="Secure password"
-                        name="Secure password"
-                        rules={[
-                            {
-                            required: false,
-                            message: 'Please input secure password',
-                            },
-                        ]} >
-                        
-                        <Input.Password value={securePW} onChange={(event)=>{ inputChanged(event, "securePW") } } />
-
-                        </Form.Item>
+                    </Form.Item>
 
 
 
-                        <Form.Item  wrapperCol={{ offset: 8,  span: 16, }} >
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                        </Form.Item>
+                    <Form.Item
+                    label="Owner of note"
+                    name="owner"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input owner name',
+                        },
+                    ]} >
+
+                    <Input allowClear  size="large"  placeholder="Owner name" value={owner} 
+                    prefix={<UserOutlined />} onChange={(event)=>{ inputChanged(event, "owner") } } />
+
+                    </Form.Item>
+
+
+                    <Form.Item
+                    label="Primary image url of note"
+                    name="Primary image url of note"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input primary image url',
+                        },
+                    ]} >
+
+                    <Search  placeholder="image url" allowClear  enterButton="Search image"  size="large" value={imageRef} 
+                    onChange={(event)=>{ inputChanged(event, "imageRef") } }/>
+
+                    </Form.Item>
+
+                    <Form.Item
+                    label="Additional image url of note "
+                    name="Additional image url of note"
+                    rules={[
+                        {
+                        required: false,
+                        message: 'Please input your additional image url !',
+                        },
+                    ]} >
+
+                    <TextArea showCount maxLength={1000} style={{ height: 120, marginBottom: 24 }} 
+                        placeholder="semicolon ';' seperated" value={addtionalImgs}onChange={(event)=>{ inputChanged(event, "addtionalImgs") } }/>
+
+
+                    </Form.Item>
+
+
+                    <Form.Item
+                    label="Reference URL"
+                    name="Reference URL"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input reference URL',
+                        },
+                    ]} >
+
+                    <Search  placeholder="reference url" allowClear  enterButton="Search image"  size="large" value={url} 
+                    onChange={(event)=>{ inputChanged(event, "url") } }/>
+
+                    </Form.Item>
+
+                    <Form.Item
+                    label="Short desc"
+                    name="Short desc"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input short desc',
+                        },
+                    ]} >
+
+                    <TextArea showCount maxLength={100} style={{ height: 120, marginBottom: 24 }} 
+                        placeholder="Short desc" value={shortDesc}onChange={(event)=>{ inputChanged(event, "shortDesc") } }/>
+
+                    </Form.Item>
+
+
+                    <Form.Item
+                    label="Full desc"
+                    name="Full desc"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input full desc',
+                        },
+                    ]} >
+
+                    <TextArea showCount maxLength={10000} style={{ height: 120, marginBottom: 24 }} 
+                        placeholder="Short desc" value={fullDesc}onChange={(event)=>{ inputChanged(event, "fullDesc") } }/>
+
+                    </Form.Item>
+
+
+                    <Form.Item
+                    label="Category"
+                    name="Category"
+                    rules={[
+                        {
+                        required: true,
+                        message: 'Please input Category',
+                        },
+                    ]} >
+
+                    <TextArea showCount maxLength={100} style={{ height: 120, marginBottom: 24 }} 
+                        placeholder="Short desc" value={category}onChange={(event)=>{ inputChanged(event, "category") } }/>
+
+                    </Form.Item>
+
+                    {/* secureInfo */}
+
+                    <Form.Item
+                    label="Secure info"
+                    name="Secure info"
+                    rules={[
+                        {
+                        required: false,
+                        message: 'Please input Secure info',
+                        },
+                    ]} >
+
+                    <TextArea showCount maxLength={10000} style={{ height: 120, marginBottom: 24 }} 
+                        placeholder="Short desc" value={secureInfo}onChange={(event)=>{ inputChanged(event, "secureInfo") } }/>
+
+                    </Form.Item>
+
+                    <Form.Item
+                    label="Secure password"
+                    name="Secure password"
+                    rules={[
+                        {
+                        required: false,
+                        message: 'Please input secure password',
+                        },
+                    ]} >
+
+                    <Input.Password value={securePW} onChange={(event)=>{ inputChanged(event, "securePW") } } />
+
+                    </Form.Item>
+
+
+
+                    <Form.Item  wrapperCol={{ offset: 8,  span: 16, }} >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    </Form.Item>
                     </Form>
-            </div>
+                </div>
+
+
+                </Col>
+                <Col xs={{ span: 24  }} lg={{ span: 6  }}>   
+
                 <div style={{  display : "flex", justifyContent : "center"}}>
                     
-                <Badge.Ribbon text="Read only Card Preview">
+                    <Badge.Ribbon text="Read only Card Preview">
+                    
+                    <FireCardForNotification  cardData={{
+                                imageRef : imageRef ,
+                                ownerAvatar : ownerAvatar,
+                                isLiked : true,
+                                title : title || "Card title",
+                                shortDesc : shortDesc || "Some short desc"
+                            }}     />
+    
+                    </Badge.Ribbon>
+    
+    
+                            
+                        <br/>
                 
-                <FireCardForNotification  cardData={{
-                            imageRef : imageRef ,
-                            ownerAvatar : ownerAvatar,
-                            isLiked : true,
-                            title : title || "Card title",
-                            shortDesc : shortDesc || "Some short desc"
-                        }}     />
-
-                </Badge.Ribbon>
-
-
-                        
-                    <br/>
-            
-                {
-
-                }
+                   
+    
+    
+                    </div>
 
 
 
-                </div>
+                </Col>
+                </Row>
 
-                </div>
                 </div>
 
             : 
